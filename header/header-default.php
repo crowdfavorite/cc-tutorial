2cd4058d2e12e9bbf1a49e9b70ebaded1f235aa1
Will Moritz <will@crowdfavorite.com>
2013-05-03 13:39:31 -0600

    a bit more

--- header/header-default.php
+++ header/header-default.php
@@ -6,19 +6,3 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 ?>
 
 <!-- Add the markup from header.php below! -->
-
-<!-- ### Default WP Style ### -->
-
-<!DOCTYPE html>
-<head>
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-
-<div class="container grid">
-	<header id="masthead" class="site-header clearfix">
-			<h1 id="site-name"><a href="<?php echo home_url('/'); ?>" title="<?php _e('Home', 'carrington-blueprint'); ?>"><?php bloginfo('name'); ?></a></h1>
-	</header><!-- #masthead -->
-
-	<div id="main" class="clearfix">
-
