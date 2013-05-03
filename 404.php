4a674554bf6b904dea2ef71f1669ee9785055f8d
Will Moritz <will@crowdfavorite.com>
2013-05-03 13:13:36 -0600

    pruning and screenshot

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
-
-// NOTE: this file is here for compatibility reasons - active templates are in the error/ dir
-
-cfct_error('404');
-
-?>
